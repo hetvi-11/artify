66ee801af38cf56e73d41e45e5bfbab711b3def0
hetvi-11 <hetvidesai4010@gmail.com>
2024-04-09 22:56:25 -0700

    project completed

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,14 +1,14 @@
-import { Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string; // URL as string, assuming no specific URL type in TypeScript
+  secureURL: string; 
   width?: number;
   height?: number;
-  config?: object; // Object represented as a Record in TypeScript
-  transformationUrl?: string;
+  config?: object; 
+  transformationUrl?: string; 
   aspectRatio?: string;
   color?: string;
   prompt?: string;
@@ -16,28 +16,28 @@ export interface IImage extends Document {
     _id: string;
     firstName: string;
     lastName: string;
-  }; // Assuming author ID as a string
-  createdAt?: Date; // Optional with default, so it's not required when creating
-  updatedAt?: Date; // Optional with default, so it's not required when creating
+  }
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
-const Image = models?.Image || model("Image", ImageSchema);
+const Image = models?.Image || model('Image', ImageSchema);
 
 export default Image;
